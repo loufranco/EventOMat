62fe98bdf69dd7c21ae08ce592b9b902a8068a09
Lou Franco <lfranco@greenwave-solutions.com>
2017-02-11 10:30:53 -0500

    Added tableview and simple cell

--- EventOMat/ViewController.swift
+++ EventOMat/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,3 +25,21 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "CellLargeText", for: indexPath)
+
+        return cell
+    }
+
+}
+
+
+extension ViewController: UITableViewDelegate {
+
+}
